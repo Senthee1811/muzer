8683018c6bf58627126920d81e93413359002728
Senthee1811 <esentheepan@gmail.com>
2025-05-19 21:40:27 +0530

    final & dumping this project!!

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,6 +1,8 @@
 import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import {z} from "zod";
+import youtubesearchapi from "youtube-search-api";
+
 const YT_REGEX = new RegExp("^https?:\/\/(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]{11})(?:&.*)?$");
 
 
@@ -26,13 +28,22 @@ export async function  POST(req:NextRequest){
      }
      const extractedId = data.url.split("?v=")[1];
 
+   const res = await youtubesearchapi.getVideo(extractedId);
+   console.log(res.title);
+   console.log(res.thumbnail.thumbnails);
+   const thumbnails = res.thumbnail.thumbnails;
+   thumbnails.sort((a:{width:Number},b:{width:Number}) => a.width < b.width ? -1 : 1);
+
      
      const stream = await prismaClient.stream.create({
        data:{
         userId: data.creatorId,
         url: data.url,
         extractedId,
-        type:"Youtube"
+        type:"Youtube",
+        title:res.title ?? "Can't Find a Video",
+        smallImg: thumbnails.length > 1 ? thumbnails(thumbnails.length-2)  ,
+        bigImg: thumbnails(thumbnails.length -1) ?? "",
        }
      })
 
@@ -62,4 +73,5 @@ export async function GET(req:NextRequest){
 
     })
     return  NextResponse.json(streams);
-}
+} 
+
